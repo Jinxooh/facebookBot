1b9eed265e4e46c21c54a4272857b9387cd6b580
황진수-PC <cjswp122@gmail.com>
2017-12-11 09:22:25 +0900

    db 스키마 세팅

--- models/review.js
+++ models/review.js
@@ -2,5 +2,30 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const Review = new Schema({
-  
-})
+  psid: String,
+  first_name: String,
+  last_name: String,
+  profile_pic: String,
+
+  reviews: { type: Array, default: [] }
+});
+
+Review.statics.create = function(psid, first_name, last_name, profile_pic) {
+  const user = new this({
+    psid, first_name, last_name, profile_pic
+  })
+  // return the Promise
+  const save = user.save()
+  return save;
+}
+
+Review.static.saveReview = function(psid, review) {
+  this.update(
+    { psid },
+    { $set: {reviews: this.reviews.push(review)}},
+    (err) => {
+      if(err) console.log('err occurs')
+      console.log('review updated!');
+    }
+  );
+}
